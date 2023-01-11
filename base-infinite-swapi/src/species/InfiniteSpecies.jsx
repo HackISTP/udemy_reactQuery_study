404e73c6370c0d6dd1b5271a77f254e1b6951b86
HackISTP <hyeminpak@gmail.com>
2023-01-11 16:11:23 +0900

    fix : useInfiniteQuery 작성하기

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,3 +1,4 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
 
@@ -9,5 +10,12 @@ const fetchUrl = async (url) => {
 
 export function InfiniteSpecies() {
   // TODO: get data for InfiniteScroll via React Query
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    "sw-people", //queryKey
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam), //쿼리함수 , pageParam 의 기본값 initialUrl
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined, //getNextPageParam 옵션주기 lastPage를 갖고있는 함수이다.
+    }
+  ); //{data, ,수집할 데이터가 더 있을지 결정(boolean)}
   return <InfiniteScroll />;
 }
